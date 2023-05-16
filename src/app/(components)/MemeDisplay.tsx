2577b9b6b0013924bef9da15eeaafd95d5e8525c
Chase Bemis <chase@godwindirect.com>
2023-05-16 16:50:17 -0400

    Stable Fetch Request 12:53

---
+++ src/app/(components)/MemeDisplay.tsx
@@ -0,0 +1,55 @@
+"use client";
+import Image from 'next/image'
+import { Anton } from 'next/font/google'
+import { useElementSize } from 'usehooks-ts';
+import { MemeTemplate } from '../(data)/types';
+
+const anton = Anton({ weight: "400", subsets: ['latin'] });
+
+const MemeDisplay = ({
+    id,
+    background,
+    textAreas,
+}: MemeTemplate 
+) => {
+    const [memeRef, { width }] = useElementSize();
+    const ratio = width / background.width;
+    console.log("🚀 ~ file: MemeDisplay.tsx:17 ~ width / background.width:", background.width)
+    console.log("🚀 ~ file: MemeDisplay.tsx:17 ~ width:", width)
+    console.log("🚀 ~ file: MemeDisplay.tsx:17 ~ ratio:", ratio)
+
+    return (
+      <div className="relative" ref={memeRef}>
+          <Image
+            src={background.src}
+            width={background.width}
+            height={background.height}
+            alt={background.alt}
+          />
+          {textAreas.map((textArea) => (
+            <div
+              key={textArea.id}
+              className="absolute"
+              style={{
+                top: textArea.top * ratio,
+                left: textArea.left * ratio,
+                width: textArea.width * ratio,
+                height: textArea.height * ratio,
+              }}
+            >
+              <div 
+                className={`${anton.className} text-center text-${textArea.color} text-stroke-${textArea.color}`}
+                style={{
+                  fontSize: textArea.fontSize * ratio,
+                  lineHeight: "1.1",
+                }}
+              >
+                {textArea.text}
+              </div>
+            </div>
+          ))} 
+      </div>
+    )
+}
+
+export default MemeDisplay
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import type { MemeTemplate } from '@/app/(data)/types';
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Anton } from 'next/font/google'
+import MemeDisplay from './(components)/MemeDisplay';
 
-const inter = Inter({ subsets: ['latin'] })
+const anton = Anton({ weight: "400", subsets: ['latin'] });
 
-export default function Home() {
+export default async function Home() {
+  const memeTemplatesReq = await fetch("http://localhost:3000/api/memeTemplates");
+  const memeTemplates = await memeTemplatesReq.json();
+  console.log(memeTemplates);
   return (
-    <main className="max-w-[1200px] mx-auto"></main>
+    <main className="max-w-[1200px] mx-auto">
+      <div className="max-w-[300px]">
+        <MemeDisplay {...memeTemplates[0]} />
+      </div>
+    </main>
   )
 }
